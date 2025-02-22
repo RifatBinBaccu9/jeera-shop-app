d0eb93bee85ca328ad5e2fa498d5a613de568295
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-02-22 22:15:20 +0600

    riad

--- app/(site)/(dashboards)/dashboard/page.js
+++ app/(site)/(dashboards)/dashboard/page.js
@@ -2,37 +2,12 @@ import OrderTable from "../ordertable/page";
 
 
 export default function Dashboard() {
-  const orders = [
-    {
-      id: 1,
-      userName: 'John Doe',
-      phoneNumber: '123-456-7890',
-      address: '123 Main St, City',
-      productName: 'Pizza',
-      productImage: '/pizza.jpg',
-      productPrice: '$15',
-      productCategory: 'Food',
-      productQuantity: 2,
-      status: 'Pending',
-    },
-    {
-      id: 2,
-      userName: 'Jane Smith',
-      phoneNumber: '987-654-3210',
-      address: '456 Elm St, City',
-      productName: 'Burger',
-      productImage: '/burger.jpg',
-      productPrice: '$8',
-      productCategory: 'Food',
-      productQuantity: 1,
-      status: 'Completed',
-    },
-  ];
+ 
 
   return (
     <div className="my-[60px]">
       <h1 className="text-2xl font-semibold mb-6 container ">Order Dashboard</h1>
-      <OrderTable orders={orders} />
+      <OrderTable />
     </div>
   );
 }
--- app/(site)/(dashboards)/ordertable/page.js
+++ app/(site)/(dashboards)/ordertable/page.js
@@ -1,10 +1,36 @@
-import React from 'react';
 
-const OrderTable = ({ orders }) => {
-  const handleDelete = (id) => {
-    // Handle delete action (could be a call to your backend API)
-    console.log('Delete order with ID:', id);
-  };
+
+const orders = [
+  {
+    id: 1,
+    userName: 'John Doe',
+    phoneNumber: '123-456-7890',
+    address: '123 Main St, City',
+    productName: 'Pizza',
+    productImage: '/pizza.jpg',
+    productPrice: '$15',
+    productCategory: 'Food',
+    productQuantity: 2,
+    status: 'Pending',
+  },
+  {
+    id: 2,
+    userName: 'Jane Smith',
+    phoneNumber: '987-654-3210',
+    address: '456 Elm St, City',
+    productName: 'Burger',
+    productImage: '/burger.jpg',
+    productPrice: '$8',
+    productCategory: 'Food',
+    productQuantity: 1,
+    status: 'Completed',
+  },
+];
+const OrderTable = () => {
+  // const handleDelete = (id) => {
+  //   // Handle delete action (could be a call to your backend API)
+  //   console.log('Delete order with ID:', id);
+  // };
 
   return (
     <div className=''>
